d7054e4b28b0f61452ece91a74be004cca4538fb
Ivan <ivanmoll07@gmail.com>
2021-09-01 00:23:51 +0200

    Add HomeComponent with article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SliderComponent
+    SliderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
