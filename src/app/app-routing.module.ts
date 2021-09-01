c02d21932822253fc477f3489c22759d3f6f35b1
Ivan <ivanmoll07@gmail.com>
2021-09-01 10:29:42 +0200

    Add blog route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
+import { HomeComponent } from "./components/home/home.component";
+import { BlogComponent } from "./components/blog/blog.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'blog', component: BlogComponent}
 ];
 
 @NgModule({
