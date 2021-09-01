d74f2fa4fc926da5c03c666f6b3e23a0a62ae7dc
Ivan <ivanmoll07@gmail.com>
2021-09-01 17:26:38 +0200

    Add to get last articles

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from "../../models/article";
+import {GetArticleService} from "../../services/http/request/get-article.service";
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  lastArticles: Article[] = [];
+
+  constructor(
+      private getArticleService: GetArticleService
+  ) { }
 
   ngOnInit(): void {
+    const lastArticles = async () => {
+      const request = this.getArticleService.send('last', '3');
+      this.lastArticles = await this.getArticleService.getBody(request);
+    }
+     lastArticles();
   }
+
+
 
 }
