00babaf33f8b8a4b1f9e7d0ad0437d81f6766fc0
Ivan <ivanmoll07@gmail.com>
2021-09-01 02:13:26 +0200

    Add GET request of last articles to HomeComponent when is init

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GetArticleService } from "../../services/get-article.service";
+import { Article } from "../../models/article";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  articles: Article[] = [];
+  lastArticles = '3';
+
+  constructor(
+      private getArticleService: GetArticleService
+  ) { }
 
   ngOnInit(): void {
+    this.getLastArticles();
   }
 
+  private async getLastArticles(): Promise<void> {
+    const response = await this.getArticleService.send('last', this.lastArticles);
+    if (response.status === 'success') {
+      this.articles = response.articles;
+    }
+  }
 }
