75f66e3f33b6c6fc9edaf532e68504eb2f6f9b5d
Ivan <ivanmoll07@gmail.com>
2021-09-01 17:15:29 +0200

    Add interfaces implementation

---
+++ src/app/services/http/request/get-article.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { ApiResponse } from "../../../models/apiResponse";
+import { GetRequest } from "./get-request";
+import { HttpClient } from "@angular/common/http";
+import { EndpointService } from "../endpoint/endpoint.service";
+import { HttpResponse } from "../response/http-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetArticleService implements GetRequest, HttpResponse {
+
+  constructor(
+      private httpClient: HttpClient,
+      private endpointService: EndpointService
+  ) {}
+
+  public send(resource: string, param: string): Promise<ApiResponse> {
+    const endpoint = this.endpointService.getEndpoint(resource, param);
+    return this.httpClient.get<ApiResponse>(endpoint).toPromise();
+  }
+
+  public async getBody(response: Promise<ApiResponse>): Promise<any> {
+    const body = await response;
+
+    if (body.status !== 'success')
+      throw new Error('');
+
+    if (body.articles.length > 0)
+      return body.articles;
+
+    if (body.article)
+      return [body.article];
+
+  }
+}
+
+
