fa0aa2a801268390fbd7729d3c92c0784561c008
Ivan <ivanmoll07@gmail.com>
2021-09-02 23:33:07 +0200

    Add create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { BlogComponent } from "./components/blog/blog.component";
+import { CreateComponent } from "./components/create/create.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'blog', component: BlogComponent}
+  {path: 'blog', component: BlogComponent},
+  {path: 'create', component: CreateComponent}
 ];
 
 @NgModule({
