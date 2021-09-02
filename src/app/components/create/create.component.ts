38e36c2cd239097c0682466e921fdd5e14fa4d67
Ivan <ivanmoll07@gmail.com>
2021-09-03 01:34:13 +0200

    Add preview image

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  imgPreview: string | undefined;
+  form: FormGroup;
+
+  constructor(
+      private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      title: new FormControl(''),
+      content: new FormControl(''),
+      image: new FormControl('')
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  onSubmit(): void {
+
+  }
+
+  previewImage(evt: any): void {
+    if (evt.target.files && evt.target.files[0]) {
+      const file = evt.target.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imgPreview = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+      // @ts-ignore
+      this.form.get('image').setValue(file);
+    }
+  }
+
+  get image(): AbstractControl { return <AbstractControl>this.form.get('image'); }
 
 }
